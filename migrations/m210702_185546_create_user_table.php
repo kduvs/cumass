42f69304448d7a5c19f083fe86a7738fffaff7e4
kduvs <vernitemnemoi2007@bk.ru>
2021-07-23 16:52:13 +0800

    обнова миграции

--- migrations/m210702_185546_create_user_table.php
+++ migrations/m210702_185546_create_user_table.php
@@ -20,12 +20,16 @@ class m210702_185546_create_user_table extends Migration
  
         $this->createTable('user', [
             'id' => $this->primaryKey(),
+            'name' => $this->string(32)->notNull(),
+            'surname' => $this->string(32)->notNull(),
+            'patronymic' => $this->string(32)->notNull(),
             'email' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'verification_token' => $this->string()->unique(),
             'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
             'status' => $this->smallinteger(6)->defaultValue(9)->notNull(),
         ], $tableOptions);
     }
